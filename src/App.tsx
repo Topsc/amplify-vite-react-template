393daf0465011f17a92b38bbd069dc39d541910c
dexter-cc <dextdz@amazon.com>
2024-07-05 17:57:30 +1000

    delete

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,10 @@ function App() {
     client.models.Todo.create({ content: window.prompt("Todo content") });
   }
 
+  function deleteTodo(id: string) {
+        client.models.Todo.delete({ id })
+    }
+
   return (
 
       <Authenticator>
@@ -28,7 +32,9 @@ function App() {
                   <button onClick={createTodo}>+ new</button>
                   <ul>
                       {todos.map((todo) => (
-                          <li key={todo.id}>{todo.content}</li>
+                          <li
+                              onClick={() => deleteTodo(todo.id)}
+                              key={todo.id}>{todo.content}</li>
                       ))}
                   </ul>
                   <div>
